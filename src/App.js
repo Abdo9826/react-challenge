223aef1671a76cbda3267cdcfed1809ccdafd95b
unknown <abodyalex1@gmail.com>
2023-03-30 12:58:37 +0300

    finish the challenge

--- src/App.js
+++ src/App.js
@@ -1,22 +1,88 @@
-import React, { useState } from 'react';
-import SingleRow from './singleRow';
-import './App.css';
+import React, { useState } from "react";
 
-export default function App() {
-  const [numRow, setNumRow] = useState([1]);
-  const addRowHandler = () => {
-    setNumRow([...numRow, numRow.length + 1]);
+function App() {
+  const [rows, setRows] = useState([{ sign: "+", value: 0, isEnabled: true }]);
+  
+  const addRow = () => {
+    setRows([...rows, { sign: "+", value: 0, isEnabled: true }]);
+  };
+
+  const removeRow = (index) => {
+    const newRows = [...rows];
+    newRows.splice(index, 1);
+    setRows(newRows);
+  };
+
+  const handleChange = (index, field, value) => {
+    const newRows = [...rows];
+    newRows[index][field] = value;
+    setRows(newRows);
+  };
+
+  const getTotal = () => {
+    let total = 0;
+    rows.forEach((row) => {
+      if (row.isEnabled) {
+        total += row.sign === "+" ? row.value : -row.value;
+      }
+    });
+    return total;
   };
 
   return (
     <div>
-      <h1>My Budget Planner</h1>
-      <button onClick={addRowHandler}>Add Row</button>
-      {numRow.map((num) => {
-        return <SingleRow  key={num} rowNumber={num} />;
-      })}
-
-      <h2>123</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Sign</th>
+            <th>Value</th>
+            <th>Enabled</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, index) => (
+            <tr key={index}>
+              <td>
+                <select
+                  value={row.sign}
+                  onChange={(event) =>
+                    handleChange(index, "sign", event.target.value)
+                  }
+                >
+                  <option value="+">+</option>
+                  <option value="-">-</option>
+                </select>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  value={row.value}
+                  onChange={(event) =>
+                    handleChange(index, "value", Number(event.target.value))
+                  }
+                />
+              </td>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={row.isEnabled}
+                  onChange={(event) =>
+                    handleChange(index, "isEnabled", event.target.checked)
+                  }
+                />
+              </td>
+              <td>
+                <button onClick={() => removeRow(index)}>Remove</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={addRow}>Add Row</button>
+      <div>Total: {getTotal()}</div>
     </div>
   );
 }
+
+export default App;
